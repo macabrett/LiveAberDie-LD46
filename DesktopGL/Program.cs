9d34a0475f78bce07eabf8267a2d95a745761fdd
Brett Story <brettmstory@gmail.com>
2020-04-25 12:54:58 -0500

    Project should run on Mac now.

--- DesktopGL/Program.cs
+++ DesktopGL/Program.cs
@@ -1,58 +1,15 @@
 ﻿namespace Macabre2D.Examples.PhysicsTest {
 
-    using Macabre2D.Framework;
     using System;
+    using Macabre2D.Framework;
 
-#if MONOMAC
-    using MonoMac.AppKit;
-    using MonoMac.Foundation;
-#endif
-
-    internal static class Program {
-        private static MacabreGame game;
-
-        internal static void RunGame() {
-            game = new MacabreGame();
-            game.Run();
-        }
-
-        /// <summary>
-        /// The main entry point for the application.
-        /// </summary>
-#if !MONOMAC
+    public static class Program {
 
         [STAThread]
-#endif
-        private static void Main(string[] args) {
-#if MONOMAC
-            NSApplication.Init ();
-
-            using (var p = new NSAutoreleasePool ()) {
-            NSApplication.SharedApplication.Delegate = new AppDelegate();
-            NSApplication.Main(args);
+        private static void Main() {
+            using (var game = new MacabreGame()) {
+                game.Run();
             }
-#else
-            RunGame();
-            Environment.Exit(0);
-#endif
         }
     }
-
-#if MONOMAC
-    internal class AppDelegate : NSApplicationDelegate {
-        public override void FinishedLaunching(MonoMac.Foundation.NSObject notification) {
-            AppDomain.CurrentDomain.AssemblyResolve += (object sender, ResolveEventArgs a) => {
-                if (a.Name.StartsWith("MonoMac")) {
-                    return typeof(MonoMac.AppKit.AppKitFramework).Assembly;
-                }
-                return null;
-            };
-            Program.RunGame();
-        }
-
-        public override bool ApplicationShouldTerminateAfterLastWindowClosed(NSApplication sender) {
-            return true;
-        }
-    }
-#endif
 }
